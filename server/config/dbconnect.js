1c5b0af98287f704acf2070b2529624eff4bdd87
Sahil wagh <sahilwagh142@gmail.com>
2024-10-07 13:38:07 +0530

    final commit - dbconnect , routers updated

--- server/config/dbconnect.js
+++ server/config/dbconnect.js
@@ -4,8 +4,6 @@ require('dotenv').config();
 const dbconnection = process.env.MONGODB_CONNECTION
 
 mongoose.connect(dbconnection,{
-    useUnifiedTopology:true,
-    useNewUrlParser:true
 }).then(()=> console.log("DataBase Connected")).catch((err)=>{
     console.log(err);
 })
